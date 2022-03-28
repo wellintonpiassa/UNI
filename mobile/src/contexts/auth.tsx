4cb982a9c37ccb87d930e70b9fe0da7aae47d2d2
Olavo Nascimento <olavo.nascimento4@gmail.com>
2022-03-28 16:23:35 -0300

    Implementar autenticação de rotas no mobile.
    Ao realizar login o aplicativo passa a armazenar o token retornado e
    armazenar no dispositivo. Permitindo que o usuário faça requisições a
    rotas que precisam de autenticação.

--- mobile/src/contexts/auth.tsx
+++ mobile/src/contexts/auth.tsx
@@ -2,24 +2,28 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { hide } from 'react-native-bootsplash';
 
-import signInService from '../services/signIn';
+import api from '../services/api';
+import signInService, { LoginStatus } from '../services/signIn';
 
 interface AuthContextData {
   isSignedIn: boolean;
-  signIn: (email: string, password: string) => Promise<boolean>;
+  signIn: (email: string, password: string) => Promise<LoginStatus>;
   signOut: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [token, setToken] = useState('');
 
   useEffect(() => {
     async function loadSavedInfo() {
-      const loggedPreviously = await AsyncStorage.getItem('@UNI:isLoggedIn');
-      setIsLoggedIn(!!loggedPreviously);
+      const loginToken = await AsyncStorage.getItem('@UNI:token');
+      if (loginToken) {
+        api.defaults.headers.common['x-access-token'] = loginToken;
+        setToken(loginToken);
+      }
       setIsLoading(false);
       setTimeout(() => {
         hide({ fade: true });
@@ -29,23 +33,25 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   async function signIn(email: string, password: string) {
-    const status = await signInService(email, password);
-    if (status) {
-      await AsyncStorage.setItem('@UNI:isLoggedIn', 'UNI');
-      setIsLoggedIn(true);
+    const { status, token: loginToken } = await signInService(email, password);
+    if (status === LoginStatus.Success) {
+      await AsyncStorage.setItem('@UNI:token', loginToken);
+      api.defaults.headers.common['x-access-token'] = loginToken;
+      setToken(loginToken);
     }
     return status;
   }
 
   async function signOut() {
-    await AsyncStorage.removeItem('@UNI:isLoggedIn');
-    setIsLoggedIn(false);
+    await AsyncStorage.removeItem('@UNI:token');
+    api.defaults.headers.common['x-access-token'] = '';
+    setToken('');
   }
 
   return (
     <AuthContext.Provider
       value={{
-        isSignedIn: !!isLoggedIn,
+        isSignedIn: !!token,
         signIn,
         signOut,
       }}>
