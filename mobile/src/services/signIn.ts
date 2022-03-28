afbaf29070df334721f43dc7fd32cba85308b19d
Olavo Nascimento <olavo.nascimento4@gmail.com>
2022-03-28 16:23:35 -0300

    Implementar autenticação de rotas no mobile.
    Ao realizar login o aplicativo passa a armazenar o token retornado e
    armazenar no dispositivo. Permitindo que o usuário faça requisições a
    rotas que precisam de autenticação.

--- mobile/src/services/signIn.ts
+++ mobile/src/services/signIn.ts
@@ -1,11 +1,35 @@
 import api from './api';
 
-async function signIn(email: string, password: string): Promise<boolean> {
-  const request = await api.post('/login', {
-    email,
-    senha: password,
-  });
-  return request.data.auth;
+export enum LoginStatus {
+  Success,
+  WrongCredentials,
+  NetworkError,
+}
+
+interface SignInInfo {
+  status: LoginStatus;
+  token: string;
+}
+
+async function signIn(email: string, password: string) {
+  let signInInfo: SignInInfo = {
+    status: LoginStatus.NetworkError,
+    token: '',
+  };
+  try {
+    const { data } = await api.post('/login', {
+      email,
+      senha: password,
+    });
+    signInInfo.status = LoginStatus.Success;
+    signInInfo.token = data.jwt;
+    return signInInfo;
+  } catch (error: any) {
+    if (error.response && !error.response.data.auth) {
+      signInInfo.status = LoginStatus.WrongCredentials;
+    }
+    return signInInfo;
+  }
 }
 
 export default signIn;
